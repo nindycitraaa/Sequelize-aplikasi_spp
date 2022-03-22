01ef79dc899985990aee6ad4965a17954a203e24
Nindy Citra Amalyani <100255467+nindycitraaa@users.noreply.github.com>
2022-03-22 11:23:41 +0700

    Add files via upload

---
+++ migrations/20220214163009-create-spp.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('spp', {
+      id_spp: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      tahun: {
+        type: Sequelize.INTEGER
+      },
+      angkatan: {
+        type: Sequelize.INTEGER
+      },
+      nominal: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('spp');
+  }
+};
